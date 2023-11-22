309a528a717b4b44f03d9a7586d4ea01cf4c145f
Dominik <dominikgos3@gmail.com>
2023-11-22 23:39:43 +0100

    added desktop navbar and main section in the app component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
 import ProfileCard from './Components/Profile/ProfileCard.vue';
 import MobileNavbar from './Components/Navbar/MobileNavbar.vue';
+import DesktopNavbar from './Components/Navbar/DesktopNavbar.vue';
 </script>
 
 <template>
   <MobileNavbar />
-  <div class="flex flex-col items-center w-screen h-screen md:p-5 lg:flex-row lg:justify-start lg:items-start">
+  <div class="flex flex-col items-center w-screen h-screen gap-10 md:p-5 lg:flex-row lg:justify-start lg:items-start">
     <ProfileCard />
+    <main class="flex flex-col gap-5 p-3 items-center w-8/12">
+      <DesktopNavbar />
+    </main>
   </div>
 </template>
---
+++ src/Components/Navbar/DesktopNavbar.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import NavbarContent from './NavbarContent.vue';
+
+</script>
+
+<template>
+  <nav class="hidden lg:flex bg-white rounded-3xl ps-6 pe-6 gap-10">
+    <NavbarContent />
+  </nav>
+</template>
