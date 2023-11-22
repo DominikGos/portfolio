92e5aa02d8145061acfb8f3f54d48dd4a6e76823
Dominik <dominikgos3@gmail.com>
2023-11-22 23:51:24 +0100

    added MainSectionWrapper component and fade transition

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,18 @@
 import ProfileCard from './Components/Profile/ProfileCard.vue';
 import MobileNavbar from './Components/Navbar/MobileNavbar.vue';
 import DesktopNavbar from './Components/Navbar/DesktopNavbar.vue';
+import MainSectionWrapper from './Components/MainSection/MainSectionWrapper.vue';
 </script>
 
 <template>
   <MobileNavbar />
-  <div class="flex flex-col items-center w-screen h-screen gap-10 md:p-5 lg:flex-row lg:justify-start lg:items-start">
+  <div
+    class="flex flex-col items-center w-screen h-screen gap-10 md:p-5 lg:flex-row lg:justify-start lg:items-start xl:p-10">
     <ProfileCard />
-    <main class="flex flex-col gap-5 p-3 items-center w-8/12">
+    <main class="flex flex-col w-full gap-10 items-center lg:w-8/12">
       <DesktopNavbar />
+      <MainSectionWrapper>
+      </MainSectionWrapper>
     </main>
   </div>
 </template>
---
+++ src/Components/MainSection/MainSectionWrapper.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="bg-white rounded-3xl p-5 w-full">
+    <Transition name="fade" appear>
+      <slot></slot>
+    </Transition>
+  </div>
+</template>
