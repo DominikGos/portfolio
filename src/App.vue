d786a00464488a0f4d5b31db9d8146f928500596
Dominik <dominikgos3@gmail.com>
2023-11-22 22:28:57 +0100

    added profile card view on large devices

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import ProfileCard from './Components/Profile/ProfileCard.vue';
 </script>
 
 <template>
-  <div class="flex flex-col items-center w-screen h-screen">
+  <div class="flex flex-col items-center w-screen h-screen md:p-5 lg:flex-row lg:justify-start lg:items-start">
     <ProfileCard />
   </div>
 </template>
